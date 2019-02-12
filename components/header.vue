54190d51c12b435fcf6dec4b030e45d61734117b
IamTinashe <tinashetp@gmail.com>
2019-02-13 02:44:15 +0300

    BurgerMenu/Fix

--- components/header.vue
+++ components/header.vue
@@ -56,13 +56,3 @@
     </router-link>
   </Slide>
 </template>
-
-<script>
-import { Slide } from 'vue-burger-menu'  // import the CSS transitions you wish to use, in this case we are using `Slide`
- 
-export default {
-    components: {
-        Slide // Register your component
-    }
-}
-</script>
