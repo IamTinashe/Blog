3b6ff0dd750e66bae45e76015db61684db5688b2
IamTinashe <tinashetp@gmail.com>
2019-02-14 22:04:39 +0300

    Edit/Profile

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,68 +1,48 @@
 <template>
   <div style="width: 100%;">
     <div class="container">
-      <div class="row position" style="margin-bottom: 20px;">
+      <div class="row" style="margin-bottom: 20px;">
         <div class="row nopadding">
           <div class="col-md-4">
             <div style="border-radius: 2px 2px 2px 2px;">
               <div
                 class="profile-image"
-                style="padding: 0px 0px 0px 0px; margin-top: 10px; margin-bottom: -50px;"
+                style="padding: 0px 0px 0px 0px; margin: 10px 0 -50px 105px;"
               >
                 <img
                   src="@/assets/tinashe.png"
                   alt="Tinashe Zvihwati"
-                  style="border: 2px solid #ffffff; width: 40%; border-radius: 100px 100px 100px 100px;"
+                  style="border: 2px solid #ffffff; width: 40%; border-radius: 120px;"
                 >
               </div>
               <div
-                class="profile-image position"
-                style="text-align: left; padding: 60px 20px 20px 30px; margin-top: 0px; margin-bottom: 40px;  background: #ffffff; border-radius: 4px 4px 4px 4px;"
+                class="profile-image"
+                style="text-align: left; padding: 60px 20px 20px 30px; margin-top: 0px; margin-bottom: 40px;  background: #ffffff; border-radius: 4px;"
               >
                 <p
                   style="color: #000000; font-weight: 600; font-size: 16px; text-align: left; padding: 5px 0 20px 0px;"
                 >Tinashe Zvihwati</p>
-								<div class="row nopadding">
-									<div class="col-md-2">
-										<i class="fa fa-id-card fa-1x" style="color: #766dff;"></i>
-									</div>
-									<div class="col-md-10 fonts-six" style="padding: 0px; margin: 0px;">
-										<p>Computer Engineer</p>
-									</div>
-								</div>
-								<div class="row nopadding">
-									<div class="col-md-2">
-										<i class="fa fa-map-marker fa-1x" style="color: #766dff;"></i>
-									</div>
-									<div class="col-md-10 fonts-six" style="padding: 0px; margin: 0px;">
-										<p>Izmir, Turkey</p>
-									</div>
-								</div>
-								<div class="row nopadding">
-									<div class="col-md-2">
-										<i class="fa fa-home fa-1x" style="color: #766dff;"></i>
-									</div>
-									<div class="col-md-10 fonts-six" style="padding: 0px; margin: 0px;">
-										<p>Harare, Zimbabwe</p>
-									</div>
-								</div>
-								<div class="row nopadding">
-									<div class="col-md-2">
-										<i class="fa fa-envelope fa-1x" style="color: #766dff;"></i>
-									</div>
-									<div class="col-md-10 fonts-six" style="padding: 0px; margin: 0px;">
-										<p>hello@iamtinashe.io</p>
-									</div>
-								</div>
-								<div class="row nopadding">
-									<div class="col-md-2">
-										<i class="fa fa-phone fa-1x" style="color: #766dff;"></i>
-									</div>
-									<div class="col-md-10 fonts-six" style="padding: 0px; margin: 0px;">
-										<p>+90 534 774 51 96</p>
-									</div>
-								</div>
-                <p class="lead" style="font-weight: 350; font-size: 14px;">
+                <div class="row">
+                  <p class="col-md-12 fonts-six">
+                    <i class="fa fa-id-card fa-1x" style="color: #766dff; width: 10%;"></i>
+                    <label>Computer Engineer</label>
+                    <br>
+                    <i class="fa fa-map-marker fa-1x" style="color: #766dff; width: 10%;"></i>
+                    <label>Izmir, Turkey</label>
+                    <br>
+                    <i class="fa fa-home fa-1x" style="color: #766dff; width: 10%;"></i>
+                    <label>Harare, Zimbabwe</label>
+                    <br>
+                    <i class="fa fa-envelope fa-1x" style="color: #766dff; width: 10%;"></i>
+                    <label>hello@iamtinashe.io</label>
+                    <br>
+                    <i class="fa fa-phone fa-1x" style="color: #766dff; width: 10%"></i>
+                    <label>+90 534 774 51 96</label>
+                  </p>
+                </div>
+                <br>
+
+                <p class="fonts-six">
                   Hello there, I'm Tinashe. Born in Marondera and grew up in Harare, Zimbabwe; I'm currently
                   based in Izmir, Turkey. I did my A Level at
                   <a
@@ -86,7 +66,8 @@
                     title="Comstice Inc."
                   >Comstice Inc.</a>
                 </p>
-                <p class="lead" style="font-weight: 350; font-size: 14px;">
+                <br>
+                <p class="fonts-six">
                   I am also a Photographer, inspired by nature and its beauty. I travel and have been to
                   9 countries and over 20 cities. I'm a big fan of cooking, not that I'm that spectacular
                   but it's a learning experience for me and I enjoy it a lot. Being a big fan of music,
@@ -97,13 +78,17 @@
           </div>
           <div
             class="col-md-8 prof-container"
-            style="background: #ffffff; margin-top: 90px; margin-bottom: 50px; padding: 20px 0 20px 0; border-radius: 3px 3px 3px 3px;"
+            style="background: #ffffff; margin-top: 92px; margin-bottom: 50px; padding: 20px 0 20px 0; border-radius: 3px 3px 3px 3px;"
           >
-            <div class="row prof-blok">
-              <div class="col-md-3">
-                <p class="fonts-seven">Work Experience</p>
-              </div>
-              <div class="col-md-9">
+            <div class="row">
+              <div class="col-md-12" style="text-align: left; padding-left: 10%;">
+                <div class="title-head">
+                  <h3 class="subtitle" style="padding-bottom: 0px;">
+                    <i class="fa fa-briefcase fa-1x"></i> <label>Work Experience</label>
+                  </h3>
+                </div>
+                <hr>
+
                 <p class="fonts-six">
                   <strong>Support Engineer</strong>
                 </p>
@@ -133,13 +118,17 @@
                 <p class="fonts-six">July 2017 - October 2017</p>
               </div>
             </div>
-            <hr>
-            <div class="row prof-blok">
-              <div class="col-md-3">
-                <p class="fonts-seven">Skills</p>
-              </div>
-              <div class="col-md-9">
-								<p class="fonts-six">Cisco UCCE, UCCX, CUCM</p>
+           
+            <div class="row" style="margin-top: 80px;">
+              <div class="col-md-12" style="text-align: left; padding-left: 10%;">
+                <div class="title-head">
+                  <h3 class="subtitle" style="padding-bottom: 0px;">
+                    <i class="fa fa-cogs fa-1x"></i> <label>Skills</label>
+                  </h3>
+                </div>
+                <hr>
+              
+                <p class="fonts-six">Cisco UCCE, UCCX, CUCM</p>
                 <p class="fonts-six">JavaScript: Nodejs, Vuejs</p>
                 <p class="fonts-six">Java</p>
                 <p class="fonts-six">Linux</p>
@@ -147,12 +136,15 @@
                 <p class="fonts-six">Microsoft Office</p>
               </div>
             </div>
-            <hr>
-            <div class="row prof-blok">
-              <div class="col-md-3">
-                <p class="fonts-seven">Education</p>
-              </div>
-              <div class="col-md-9">
+
+            <div class="row" style="margin-top: 80px;">
+              <div class="col-md-12" style="text-align: left; padding-left: 10%;">
+                <div class="title-head">
+                  <h3 class="subtitle" style="padding-bottom: 0px;">
+                    <i class="fa fa-graduation-cap fa-1x"></i> <label>Education</label>
+                  </h3>
+                </div>
+                <hr>
                 <p class="fonts-six">
                   <strong>Bsc. in Computer Engineering</strong>
                 </p>
@@ -195,35 +187,34 @@
                 <p class="fonts-six">March 2011 - December 2012</p>
               </div>
             </div>
-						<hr>
-						<div class="row prof-blok">
-              <div class="col-md-3">
+            <hr>
+            <div class="row">
+              <div style="width: 30%;">
                 <p class="fonts-seven">Languages</p>
               </div>
-              <div class="col-md-9">
+              <div style="width: 70%;">
                 <p class="fonts-six">English, Shona, Turkish</p>
-							</div>
-						</div>
-						<hr>
-						<div class="row prof-blok">
-              <div class="col-md-3">
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div style="width: 30%;">
                 <p class="fonts-seven">Travelled</p>
               </div>
-              <div class="col-md-9">
+              <div style="width: 70%;">
                 <p class="fonts-six">
-                      South Africa,
-                      United Arab Emirates,
-                      Turkey,
-                      Namibia,
-                      Qatar,
-                      Egypt,
-                      Zambia,
-                      Botswana,
-                      Ethiopia
-                    </p>
-							</div>
-						</div>
-            
+                  South Africa,
+                  United Arab Emirates,
+                  Turkey,
+                  Namibia,
+                  Qatar,
+                  Egypt,
+                  Zambia,
+                  Botswana,
+                  Ethiopia
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
