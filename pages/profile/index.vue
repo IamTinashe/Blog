599ba4f597edfbf4abeb5faa3e13fc3b2482ef06
IamTinashe <tinashetp@gmail.com>
2019-02-14 18:30:39 +0300

    Page/Profile

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,175 +1,239 @@
 <template>
-  <div class="position" style="width: 100%;">
-    <div
-      class="row position"
-      style="padding-left: 0px; padding-right: 0px; margin-left: 0px; margin-right: 0px; width: 100%; margin-top: 100px;"
-    >
-      <div
-        class="row position"
-        style="padding-left: 0px; padding-right: 0px; margin-left: 0px; margin-right: 0px; width: 100%; height: 100%; z-index: -1; top: 0px;">
-	  </div>
-			<div class="row nopadding" style="width: 80%;">
-				<div class="col-md-4">
-					<div style="padding: 0px 0px 0px 0px; border-radius: 2px 2px 2px 2px;">
-						<div class="profile-image position" style="padding: 0px 0px 0px 0px; margin-top: 10px; margin-bottom: -50px;">
-							<img src="@/assets/tinashe.png" alt="Tinashe Zvihwati" style="border: 2px solid #ffffff; width: 40%; border-radius: 100px 100px 100px 100px;">
-						</div>
-						<div class="profile-image position" style="text-align: left; padding: 60px 20px 20px 30px; margin-top: 0px; margin-bottom: 40px;  background: #ffffff; border-radius: 4px 4px 4px 4px;">
-							<p style="color: #000000; font-weight: 600; font-size: 16px; text-align: left; padding: 5px 0 20px 0px;">Tinashe Zvihwati</p>
-							<p class="lead" style="font-weight: 350; font-size: 14px;">
-								Hello there, I'm Tinashe. Born in Marondera and grew up in Harare, Zimbabwe; I'm currently
-								based in Izmir, Turkey. I did my A Level at
-								<a href="http://www.gatewayhigh.co.zw/" target="_blank" rel="noopener" title="Gateway High School" >Gateway High School</a>
-								and furthered my studies at
-								<a href="http://www.iyte.edu.tr/" target="_blank" rel="noopener" title="Izmir Institute of Technology" >Izmir Institute of Technology</a>
-								where I graduated with a BSc. in Computer Engineering. I currently work as a Support Engineer at
-								<a href="https://www.comstice.com/" target="_blank" rel="noopener" title="Comstice Inc." >Comstice Inc.</a>
-							</p>
-							<p class="lead" style="font-weight: 350; font-size: 14px;">
-								I am also a Photographer, inspired by nature and its beauty. I travel and have been to
-								9 countries and over 20 cities. I'm a big fan of cooking, not that I'm that spectacular
-								but it's a learning experience for me and I enjoy it a lot. Being a big fan of music,
-								I make Rap an House beats too.
-							</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-8 prof-container" style="background: #ffffff; margin-top: 80px; margin-bottom: 50px; padding: 20px 0 20px 0; border-radius: 3px 3px 3px 3px;">
-					<div class="row prof-blok">
-						<div class="md-col-5" style="width: 20%; margin-left: 15px;">
-							<p class="fonts-seven">Bio</p>
-						</div>
-						<div class="md-col-6" style="width: 70%;">
-							<div class="row" style="margin-left: 0px;">
-								<div style="width: 30%;">
-									<p class="fonts-six">Full Name:</p>
+  <div style="width: 100%;">
+    <div class="container">
+      <div class="row position" style="margin-bottom: 20px;">
+        <div class="row nopadding">
+          <div class="col-md-4">
+            <div style="border-radius: 2px 2px 2px 2px;">
+              <div
+                class="profile-image"
+                style="padding: 0px 0px 0px 0px; margin-top: 10px; margin-bottom: -50px;"
+              >
+                <img
+                  src="@/assets/tinashe.png"
+                  alt="Tinashe Zvihwati"
+                  style="border: 2px solid #ffffff; width: 40%; border-radius: 100px 100px 100px 100px;"
+                >
+              </div>
+              <div
+                class="profile-image position"
+                style="text-align: left; padding: 60px 20px 20px 30px; margin-top: 0px; margin-bottom: 40px;  background: #ffffff; border-radius: 4px 4px 4px 4px;"
+              >
+                <p
+                  style="color: #000000; font-weight: 600; font-size: 16px; text-align: left; padding: 5px 0 20px 0px;"
+                >Tinashe Zvihwati</p>
+								<div class="row nopadding">
+									<div class="col-md-2">
+										<i class="fa fa-id-card fa-1x" style="color: #766dff;"></i>
+									</div>
+									<div class="col-md-10 fonts-six" style="padding: 0px; margin: 0px;">
+										<p>Computer Engineer</p>
+									</div>
 								</div>
-								<div style="width: 70%;">
-									<p class="fonts-six">Tinashe Zvihwati</p>
+								<div class="row nopadding">
+									<div class="col-md-2">
+										<i class="fa fa-map-marker fa-1x" style="color: #766dff;"></i>
+									</div>
+									<div class="col-md-10 fonts-six" style="padding: 0px; margin: 0px;">
+										<p>Izmir, Turkey</p>
+									</div>
 								</div>
-							</div>
-							<div class="row" style="margin-left: 0px;">
-								<div style="width: 30%;">
-									<p class="fonts-six">Profession:</p>
+								<div class="row nopadding">
+									<div class="col-md-2">
+										<i class="fa fa-home fa-1x" style="color: #766dff;"></i>
+									</div>
+									<div class="col-md-10 fonts-six" style="padding: 0px; margin: 0px;">
+										<p>Harare, Zimbabwe</p>
+									</div>
 								</div>
-								<div style="width: 70%;">
-									<p class="fonts-six">Computer Engineer</p>
+								<div class="row nopadding">
+									<div class="col-md-2">
+										<i class="fa fa-envelope fa-1x" style="color: #766dff;"></i>
+									</div>
+									<div class="col-md-10 fonts-six" style="padding: 0px; margin: 0px;">
+										<p>hello@iamtinashe.io</p>
+									</div>
 								</div>
-							</div>
-							<div class="row" style="margin-left: 0px;">
-								<div style="width: 30%;">
-									<p class="fonts-six">Nationality:</p>
+								<div class="row nopadding">
+									<div class="col-md-2">
+										<i class="fa fa-phone fa-1x" style="color: #766dff;"></i>
+									</div>
+									<div class="col-md-10 fonts-six" style="padding: 0px; margin: 0px;">
+										<p>+90 534 774 51 96</p>
+									</div>
 								</div>
-								<div style="width: 70%;">
-									<p class="fonts-six">Zimbabwean</p>
-								</div>
-							</div>
-							<div class="row" style="margin-left: 0px;">
-								<div style="width: 30%;">
-									<p class="fonts-six">Current:</p>
-								</div>
-								<div style="width: 70%;">
-									<p class="fonts-six">Izmir, Turkey</p>
-								</div>
-							</div>
-							<div class="row" style="margin-left: 0px;">
-								<div style="width: 30%;">
-									<p class="fonts-six">Languages:</p>
-								</div>
-								<div style="width: 70%;">
-									<p class="fonts-six">English, Shona, Turkish</p>
-								</div>
-							</div>
-							<div class="row" style="margin-left: 0px;">
-								<div style="width: 30%;">
-									<p class="fonts-six">Travelled:</p>
-								</div>
-								<div style="width: 70%;">
-									<p class="fonts-six">
-										South Africa,
-										United Arab Emirates,
-										Turkey,
-										Namibia,
-										Qatar,
-										Egypt,
-										Zambia,
-										Botswana,
-										Ethiopia
-									</p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<hr>
-					<div class="row prof-blok">
-						<div class="md-col-5" style="width: 20%; margin-left: 15px;">
-							<p class="fonts-seven">Work Experience</p>
-						</div>
-						<div class="md-col-6" style="width: 70%;">
-							<p class="fonts-six"><strong>Support Engineer</strong></p>
-							<a href="https://www.comstice.com/" target="_blank" rel="noopener" title="Comstice Inc." >
-								<p class="fonts-six externalLink">Comstice Inc.</p>
-							</a>
-							<p class="fonts-six">Izmir, Turkey</p>
-							<p class="fonts-six">March 2018 - Present</p>
-							<br>
-							<p class="fonts-six"><strong>Internship</strong></p>
-							<a href="http://www.wua.ac.zw/" target="_blank" rel="noopener" title="Women's University in Africa" >
-								<p class="fonts-six externalLink">Women's University in Africa</p>
-							</a>
-							<p class="fonts-six">Harare, Zimbabwe</p>
-							<p class="fonts-six">July 2017 - October 2017</p>
-						</div>
-					</div>
-					<hr>
-					<div class="row prof-blok">
-						<div class="md-col-5" style="width: 20%; margin-left: 15px;">
-							<p class="fonts-seven">Skills</p>
-						</div>
-						<div class="md-col-6" style="width: 70%;">
-							<p class="fonts-six">JavaScript: Nodejs, Vuejs</p>
-							<p class="fonts-six">Java</p>
-							<p class="fonts-six">Linux</p>
-							<p class="fonts-six">HTML, CSS, Bootstrap</p>
-							<p class="fonts-six">Microsoft Office</p>
-						</div>
-					</div>
-					<hr>
-					<div class="row prof-blok">
-						<div class="md-col-5" style="width: 20%; margin-left: 15px;">
-							<p class="fonts-seven">Education</p>
-						</div>
-						<div class="md-col-6" style="width: 70%;">
-							<p class="fonts-six"><strong>Bsc. in Computer Engineering</strong></p>
-							<a href="http://www.iyte.edu.tr/" target="_blank" rel="noopener" title="Izmir Institute of Technology" >
-								<p class="fonts-six externalLink">Izmir Institute of Technology</p>
-							</a>
-							<p class="fonts-six">September 2014 - August 2018</p>
-							<br>
+                <p class="lead" style="font-weight: 350; font-size: 14px;">
+                  Hello there, I'm Tinashe. Born in Marondera and grew up in Harare, Zimbabwe; I'm currently
+                  based in Izmir, Turkey. I did my A Level at
+                  <a
+                    href="http://www.gatewayhigh.co.zw/"
+                    target="_blank"
+                    rel="noopener"
+                    title="Gateway High School"
+                  >Gateway High School</a>
+                  and furthered my studies at
+                  <a
+                    href="http://www.iyte.edu.tr/"
+                    target="_blank"
+                    rel="noopener"
+                    title="Izmir Institute of Technology"
+                  >Izmir Institute of Technology</a>
+                  where I graduated with a BSc. in Computer Engineering. I currently work as a Support Engineer at
+                  <a
+                    href="https://www.comstice.com/"
+                    target="_blank"
+                    rel="noopener"
+                    title="Comstice Inc."
+                  >Comstice Inc.</a>
+                </p>
+                <p class="lead" style="font-weight: 350; font-size: 14px;">
+                  I am also a Photographer, inspired by nature and its beauty. I travel and have been to
+                  9 countries and over 20 cities. I'm a big fan of cooking, not that I'm that spectacular
+                  but it's a learning experience for me and I enjoy it a lot. Being a big fan of music,
+                  I make Rap an House beats too.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-md-8 prof-container"
+            style="background: #ffffff; margin-top: 90px; margin-bottom: 50px; padding: 20px 0 20px 0; border-radius: 3px 3px 3px 3px;"
+          >
+            <div class="row prof-blok">
+              <div class="col-md-3">
+                <p class="fonts-seven">Work Experience</p>
+              </div>
+              <div class="col-md-9">
+                <p class="fonts-six">
+                  <strong>Support Engineer</strong>
+                </p>
+                <a
+                  href="https://www.comstice.com/"
+                  target="_blank"
+                  rel="noopener"
+                  title="Comstice Inc."
+                >
+                  <p class="fonts-six externalLink">Comstice Inc.</p>
+                </a>
+                <p class="fonts-six">Izmir, Turkey</p>
+                <p class="fonts-six">March 2018 - Present</p>
+                <br>
+                <p class="fonts-six">
+                  <strong>Internship</strong>
+                </p>
+                <a
+                  href="http://www.wua.ac.zw/"
+                  target="_blank"
+                  rel="noopener"
+                  title="Women's University in Africa"
+                >
+                  <p class="fonts-six externalLink">Women's University in Africa</p>
+                </a>
+                <p class="fonts-six">Harare, Zimbabwe</p>
+                <p class="fonts-six">July 2017 - October 2017</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row prof-blok">
+              <div class="col-md-3">
+                <p class="fonts-seven">Skills</p>
+              </div>
+              <div class="col-md-9">
+								<p class="fonts-six">Cisco UCCE, UCCX, CUCM</p>
+                <p class="fonts-six">JavaScript: Nodejs, Vuejs</p>
+                <p class="fonts-six">Java</p>
+                <p class="fonts-six">Linux</p>
+                <p class="fonts-six">HTML, CSS, Bootstrap</p>
+                <p class="fonts-six">Microsoft Office</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row prof-blok">
+              <div class="col-md-3">
+                <p class="fonts-seven">Education</p>
+              </div>
+              <div class="col-md-9">
+                <p class="fonts-six">
+                  <strong>Bsc. in Computer Engineering</strong>
+                </p>
+                <a
+                  href="http://www.iyte.edu.tr/"
+                  target="_blank"
+                  rel="noopener"
+                  title="Izmir Institute of Technology"
+                >
+                  <p class="fonts-six externalLink">Izmir Institute of Technology</p>
+                </a>
+                <p class="fonts-six">September 2014 - August 2018</p>
+                <br>
 
-							<p class="fonts-six"><strong>Turkish as a Foreign Langauge</strong></p>
-							<a href="http://dedam.deu.edu.tr/en/" target="_blank" rel="noopener" title="Dokuz Eylul University" >
-								<p class="fonts-six externalLink">Dokuz Eylul University</p>
-							</a>
-							<p class="fonts-six">October 2013 - August 2014</p>
-							<br>
+                <p class="fonts-six">
+                  <strong>Turkish as a Foreign Langauge</strong>
+                </p>
+                <a
+                  href="http://dedam.deu.edu.tr/en/"
+                  target="_blank"
+                  rel="noopener"
+                  title="Dokuz Eylul University"
+                >
+                  <p class="fonts-six externalLink">Dokuz Eylul University</p>
+                </a>
+                <p class="fonts-six">October 2013 - August 2014</p>
+                <br>
 
-							<p class="fonts-six"><strong>Advanced Level (High School)</strong></p>
-							<a href="http://www.gatewayhigh.co.zw/" target="_blank" rel="noopener" title="Gateway High School" >
-								<p class="fonts-six externalLink">Gateway High School</p>
-							</a>
-							<p class="fonts-six">March 2011 - December 2012</p>
+                <p class="fonts-six">
+                  <strong>Advanced Level (High School)</strong>
+                </p>
+                <a
+                  href="http://www.gatewayhigh.co.zw/"
+                  target="_blank"
+                  rel="noopener"
+                  title="Gateway High School"
+                >
+                  <p class="fonts-six externalLink">Gateway High School</p>
+                </a>
+                <p class="fonts-six">March 2011 - December 2012</p>
+              </div>
+            </div>
+						<hr>
+						<div class="row prof-blok">
+              <div class="col-md-3">
+                <p class="fonts-seven">Languages</p>
+              </div>
+              <div class="col-md-9">
+                <p class="fonts-six">English, Shona, Turkish</p>
+							</div>
 						</div>
-					</div>
-				</div>
-			</div> 
+						<hr>
+						<div class="row prof-blok">
+              <div class="col-md-3">
+                <p class="fonts-seven">Travelled</p>
+              </div>
+              <div class="col-md-9">
+                <p class="fonts-six">
+                      South Africa,
+                      United Arab Emirates,
+                      Turkey,
+                      Namibia,
+                      Qatar,
+                      Egypt,
+                      Zambia,
+                      Botswana,
+                      Ethiopia
+                    </p>
+							</div>
+						</div>
+            
+          </div>
+        </div>
+      </div>
     </div>
-	</div>
+  </div>
 </template>
 
 <style>
-.externalLink:hover{
-	color: #000000;
-	font-weight: 350;
+.externalLink:hover {
+  color: #000000;
+  font-weight: 350;
 }
 </style>
